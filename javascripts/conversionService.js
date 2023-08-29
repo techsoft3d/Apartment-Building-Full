244b19f0d1e06c67ad25995e7a4b87894d60c3d0
Ricky Johnson <ricky.johnson@techsoft3d.com>
2023-08-29 11:22:30 -0400

    added functionality for hc versioning

--- javascripts/conversionService.js
+++ javascripts/conversionService.js
@@ -36,3 +36,79 @@ async function startViewer() {
 
 }
 
+async function fetchVersionNumber() {
+        const conversionServiceURI = "https://csapi.techsoft3d.com";
+
+        let res = await fetch(conversionServiceURI + '/api/hcVersion');
+        var data = await res.json();
+        versionNumer = data;
+        
+        return data
+
+}
+
+
+
+async function initializeViewer() {
+        var viewer = null;
+    var ui = null;
+    var INITIAL_CAMERA = {
+      position: {
+        x: -111038.50704000151,
+        y: -50454.77499068544,
+        z: 35699.247383553375,
+      },
+      target: {
+        x: -44.93310546875,
+        y: 5220.4443359375,
+        z: 22040,
+      },
+      up: {
+        x: 0.10243505924246869,
+        y: 0.039640069717750984,
+        z: 0.9939495578301567,
+      },
+      width: 70000,
+      height: 70000,
+      projection: 0,
+      nearLimit: 0,
+      cameraFlags: 0,
+    };
+
+    const models = ["Arboleda_Bldg"];
+
+
+    var date = new Date();
+    var start = date.getTime();
+
+    viewer = await startViewer()
+
+    viewer.setCallbacks({
+      sceneReady: function () {
+        viewer
+          .getView()
+          .setCamera(Communicator.Camera.fromJson(INITIAL_CAMERA));
+        viewer.getView().setBackfacesVisible(true);
+
+      },
+      modelStructureReady: function () {
+        var date = new Date();
+        var end = date.getTime();
+        viewer.view.setBackgroundColor(new Communicator.Color(0, 0, 0));
+
+        console.log("Load time = " + (end - start) / 1000.0 + " seconds.");
+      },
+    });
+
+    const uiConfig = {
+      containerId: "content",
+      screenConfiguration: Sample.screenConfiguration,
+    };
+    ui = new Communicator.Ui.Desktop.DesktopUi(viewer, uiConfig);
+
+
+    window.onresize = function () {
+      viewer.resizeCanvas();
+    };
+
+}
